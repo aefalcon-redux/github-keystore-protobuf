7eafca836bb8d9241995c3b7e2486f6661945e35
Andrew E. Falcon <aefalcon@gmail.com>
2018-09-17 23:12:28 +0000

    Initial protocol for token management

--- dist.mk
+++ dist.mk
@@ -3,12 +3,15 @@ PYTHON ?= python3
 
 .PHONY: all clean
 
-all: go/appkeypb/appkey.pb.go py/appkey_pb2.py go/appkeypb/static.go
+all: go/appkeypb/appkey.pb.go py/appkey_pb2.py go/appkeypb/static.go go/tokenpb/token.pb.go py/token_pb2.py
 
 go/appkeypb/appkey.pb.go py/appkey_pb2.py: appkey.proto go/appkeypb py
 	$(PROTOC) --python_out=py --go_out=go/appkeypb $<
 
-py go go/appkeypb:
+go/tokenpb/token.pb.go py/token_pb2.py: token.proto go/tokenpb py
+	$(PROTOC) --python_out=py --go_out=go/tokenpb $<
+
+py go go/appkeypb go/tokenpb:
 	mkdir -p $@
 
 links.pbf go/appkeypb/static.go py/appkey_data.py: links.json appkey.proto generate.py static.go.tmpl appkey_data.py.tmpl
